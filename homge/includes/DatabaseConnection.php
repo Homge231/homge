741b4ba367c4eb498ef7dac97971b716f6f60ee4
Homge231 <163499225+Homge231@users.noreply.github.com>
2024-10-11 22:29:52 +0700

    Delete homge directory

--- homge/includes/DatabaseConnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=homge;charset=utf8mb4', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die('Database connection failed: ' . $e->getMessage());
-}
-?>
